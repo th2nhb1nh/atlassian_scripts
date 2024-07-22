5f38345ede15e386ad0fe55a2d6335c9e561a3e3
th2nhb1nh <thanhbinh.pham610@gmail.com>
2024-07-22 17:48:37 +0700

    add function to get first characters of 3 words

--- README.md
+++ README.md
@@ -1,5 +1,7 @@
 # atlassian_scripts
 
-Get Jira Workspace ID by: 
-`https://<JSM Premium Site Name>.atlassian.net/rest/servicedeskapi/assets/workspace`
+Get Jira Workspace ID by:
 
+```
+https://<JSM Premium Site Name>.atlassian.net/rest/servicedeskapi/assets/workspace
+```
--- utils/string_handling.py
+++ utils/string_handling.py
@@ -1,4 +1,10 @@
+# utils/string_handling.py
 import base64
 
 def encode_base64(str):
     return base64.b64encode(str.encode('utf-8')).decode('utf-8')
+
+def get_initials(string):
+    words = string.split()
+    initials = ''.join(word[0].upper() for word in words[:3])
+    return initials
